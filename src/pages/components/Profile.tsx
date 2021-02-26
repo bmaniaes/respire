4e76349514d0829ca71e754a98c424a1514d3eb0
Breno Maniaes <bmaniaes@gmail.com>
2021-02-26 11:39:58 -0300

    :tada: initial commit

---
+++ src/pages/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../../contexts/Challenges.Context';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/bmaniaes.png" alt="Breno Maniaes" />
+            <div>
+                <strong>Breno Maniaes</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level" />
+                    Level { level }
+                </p>
+            </div>
+        </div>
+    );
+}
